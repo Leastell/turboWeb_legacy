311ad4c34495ba5e96785c4aa08663b216832f86
Libruh <laif11c@gmail.com>
2020-11-10 18:40:33 -0600

    Fixed error where a track would error without a contributor.

--- src/components/track.jsx
+++ src/components/track.jsx
@@ -18,9 +18,10 @@ class trackView extends Component {
             <div className="track_info">
                 { <div className='track_name'> {track.name} </div> }
                 { <div className='artist_name'> {track.album.artists[0].name} </div> }
+                { track.contributor ?
                 <Link to={"/music/user/"+track.contributor.user.id} className="no_link">
-                    { track.contributor ? <DiscordUser userObj={track.contributor} />: <div></div>}
-                </Link>
+                    <DiscordUser userObj={track.contributor} />
+                </Link> : <div></div> }
             </div>
             <div className='vote_button'>
                 <VoteButton trackID={track.id} votes={track.votes} voteStatus={this.props.voteStatus} storageFunction = {this.props.storageFunction}/>
