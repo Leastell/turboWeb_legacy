fa9b0742fa0864194117b47167007cca2c71bcba
BuildTools <laif11c@gmail.com>
2021-03-08 17:14:35 -0600

    fixed start file

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-screen -S "Turbo Website" serve -s build -n
+sudo screen -S "Turbo Website" serve -s -n build --ssl-cert "/etc/letsencrypt/live/turboaf.net-0001/fullchain.pem" --ssl-key "/etc/letsencrypt/live/turboaf.net-0001/privkey.pem"
